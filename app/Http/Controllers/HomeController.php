3540b6020cef533f1ae03be5d29eef1054f2a519
ZR Saimun <zrsaimun@gmail.com>
2021-01-12 22:35:22 +0600

    merge controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-
+use App\Models\retailsellerModel;
 use App\Models\sellerModel;
 use Illuminate\Support\Facades\Auth;
 
